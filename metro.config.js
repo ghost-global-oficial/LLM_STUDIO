83f03ebb08d6be19cf33fd85af2ae8ddad5968a7
Ghost <ghost.sytems@gmail.com>
2026-07-09 11:51:52 +0100

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
-const path = require('path');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.assetExts.push('ttf', 'otf', 'woff', 'woff2');
-config.projectRoot = path.resolve(__dirname);
-
-module.exports = config;
